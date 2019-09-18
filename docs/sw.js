4e24e35a6ef0adb6e4e045d8b88535aef8b9583c
Nathan <khangntn@ibshcm.edu.vn>
2019-09-18 17:55:42 +0700

    adding website files to be hosted

---
+++ docs/sw.js
@@ -0,0 +1,89 @@
+const staticCacheName = "site-static-v4";
+const dynamicCacheName = "site-dynamic-v4";
+const assets = [
+  "/",
+  "/index.html",
+  "/genindex.html",
+  "/search.html",
+  "/csfdoc.html",
+  "/_static/custom.css",
+  "/_static/alabaster.css",
+  "/_static/pygments.css",
+  "/_static/jquery.js",
+  "/_static/underscore.js",
+  "/_static/doctools.js",
+  "_static/images/",
+  "/_static/materialize.min.js",
+  "/_static/styles.css",
+  "/_static/materialize.min.css",
+  "/fallback.html"
+];
+
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
+// install event
+self.addEventListener("install", evt => {
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      console.log("caching shell assets");
+      //cache.addAll(assets);
+      cache
+        .addAll(assets)
+        .then(() => {})
+        .catch(error => console.error(`Oops! ${error}`));
+    })
+  );
+});
+
+// activate event
+self.addEventListener("activate", evt => {
+  //console.log('service worker activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      //console.log(keys);
+      return Promise.all(
+        keys
+          .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// fetch events
+self.addEventListener("fetch", evt => {
+  if (evt.request.url.indexOf("firestore.googleapis.com") === -1) {
+    evt.respondWith(
+      caches
+        .match(evt.request)
+        .then(cacheRes => {
+          return (
+            cacheRes ||
+            fetch(evt.request).then(fetchRes => {
+              return caches.open(dynamicCacheName).then(cache => {
+                cache.put(evt.request.url, fetchRes.clone());
+                // check cached items size
+                //limitCacheSize(dynamicCacheName, 15);
+                return fetchRes;
+              });
+            })
+          );
+        })
+        .catch(() => {
+          if (evt.request.url.indexOf(".html") > -1) {
+            return caches.match("/fallback.html");
+          }
+        })
+    );
+  }
+});
